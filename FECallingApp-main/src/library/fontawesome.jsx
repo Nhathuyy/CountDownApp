b1df97e733fd73afe7dee72101976aa85a6d7d98
huybutter <88093536+Nhathuyy@users.noreply.github.com>
2022-11-20 19:01:50 +0700

    Add files via upload

---
+++ FECallingApp-main/src/library/fontawesome.jsx
@@ -0,0 +1,48 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { 
+    faMagnifyingGlass, 
+    faCheck, 
+    faArrowLeft, 
+    faRightLong, 
+    faEye, 
+    faHeart, 
+    faUser, 
+    faForward,
+    faUserGroup,
+    faPaperPlane,
+    faBars,
+    faRightFromBracket,
+    faPlus,
+    faCommentMedical,
+    faUsersLine,
+    faPeopleGroup,
+    faFaceSmile,
+    faVolumeHigh,
+    faVolumeXmark,
+    faImage
+} from '@fortawesome/free-solid-svg-icons'
+library.add(
+    fab,
+    faMagnifyingGlass, 
+    faCheck, 
+    faArrowLeft, 
+    faRightLong, 
+    faEye, 
+    faHeart, 
+    faUser, 
+    faForward,
+    faUserGroup,
+    faPaperPlane,
+    faBars,
+    faRightFromBracket,
+    faPlus,
+    faCommentMedical,
+    faUsersLine,
+    faPeopleGroup,
+    faFaceSmile,
+    faVolumeHigh,
+    faVolumeXmark,
+    faImage
+)
